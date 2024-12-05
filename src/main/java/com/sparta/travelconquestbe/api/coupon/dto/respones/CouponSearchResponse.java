21866b2cf64b41c178182c8aaa3979e5e5d8edba
Younghan Yonn <dudgks1172@naver.com>
2024-12-05 20:26:15 +0900

    [#10] feat : 쿠폰 목록 조회 기능 구현 - Querydsl 사용

--- src/main/java/com/sparta/travelconquestbe/api/coupon/dto/respones/CouponSearchResponse.java
+++ src/main/java/com/sparta/travelconquestbe/api/coupon/dto/respones/CouponSearchResponse.java
@@ -11,9 +11,21 @@ public class CouponSearchResponse {
     private String name;
     private String description;
     private String code;
-    private int discount_amount;
-    private LocalDate valid_until;
+    private int discountAmount;
+    private LocalDate validUntil;
     private int count;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public CouponSearchResponse(Long id, String name, String description, String code, int discountAmount, LocalDate validUntil, int count, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.code = code;
+        this.discountAmount = discountAmount;
+        this.validUntil = validUntil;
+        this.count = count;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
---
+++ src/main/java/com/sparta/travelconquestbe/api/coupon/service/CouponService.java
@@ -0,0 +1,21 @@
+package com.sparta.travelconquestbe.api.coupon.service;
+
+import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponSearchResponse;
+import com.sparta.travelconquestbe.domain.coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+    private final CouponRepository couponRepository;
+
+    @Transactional(readOnly = true)
+    public Page<CouponSearchResponse> couponSearch(int page, int limit) {
+
+        return couponRepository.searchCoupons(PageRequest.of(page - 1, limit));
+    }
+}
