c04b56c47500dace5dd801ce0ba73ee7f317c093
Younghan Yonn <dudgks1172@naver.com>
2024-12-06 16:59:01 +0900

    [#10] feat : 쿠폰 저장 기능 추가 -

--- src/main/java/com/sparta/travelconquestbe/api/coupon/controller/CouponController.java
+++ src/main/java/com/sparta/travelconquestbe/api/coupon/controller/CouponController.java
@@ -1,15 +1,14 @@
 package com.sparta.travelconquestbe.api.coupon.controller;
 
+import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponSaveResponse;
 import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponSearchResponse;
 import com.sparta.travelconquestbe.api.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/coupons")
@@ -24,5 +23,15 @@ public class CouponController {
     ) {
         Page<CouponSearchResponse> response = couponService.searchAllCoupons(page, limit);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<CouponSaveResponse> saveCoupon(
+            @PathVariable(name = "id") Long couponId,
+            @AuthenticationPrincipal AuthUser user
+    ) {
+        Long userId = user.getId();
+        CouponSaveResponse response = couponService.saveCoupon(couponId, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
