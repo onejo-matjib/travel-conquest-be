c895bc4b3d794e965f9a2f393d57979391c3abcc
92jy38 <92jy38@gmail.com>
2024-12-08 23:56:01 +0900

    [#19]feat: 즐겨찾기 리스트 조회 기능 추가

--- src/main/java/com/sparta/travelconquestbe/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/sparta/travelconquestbe/domain/bookmark/repository/BookmarkRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.travelconquestbe.domain.bookmark.repository;
 
 import com.sparta.travelconquestbe.domain.bookmark.entity.Bookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
   @Query("SELECT EXISTS (SELECT 1 FROM Bookmark b WHERE b.user.id = :userId AND b.route.id = :routeId)")
   boolean isBookmarkExist(@Param("userId") Long userId, @Param("routeId") Long routeId);
+
+  Page<Bookmark> findAllByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 }
