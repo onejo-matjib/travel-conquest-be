63a4b36cf435902e0b48a25d52d0ce8a9331e7a0
92jy38 <92jy38@gmail.com>
2024-12-08 23:12:19 +0900

    [#19]chore: 성능 최적화 - COUNT 대신 EXIST 사용

--- src/main/java/com/sparta/travelconquestbe/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/sparta/travelconquestbe/domain/bookmark/repository/BookmarkRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-  @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END " +
-      "FROM Bookmark b WHERE b.user.id = :userId AND b.route.id = :routeId")
+  @Query("SELECT EXISTS (SELECT 1 FROM Bookmark b WHERE b.user.id = :userId AND b.route.id = :routeId)")
   boolean isBookmarkExist(@Param("userId") Long userId, @Param("routeId") Long routeId);
 }
