d4d062289f713b28cc2f590b7d3391dae14633f2
92jy38 <92jy38@gmail.com>
2024-12-10 21:54:42 +0900

    [#27]chore: 에러코드 수정

--- src/main/java/com/sparta/travelconquestbe/domain/review/entity/Review.java
+++ src/main/java/com/sparta/travelconquestbe/domain/review/entity/Review.java
@@ -57,7 +57,7 @@ public class Review extends TimeStampCreated {
 
   public void validateOwner(Long userId) {
     if (!this.user.getId().equals(userId)) {
-      throw new CustomException("REVIEW_003", "본인의 리뷰만 삭제할 수 있습니다.", HttpStatus.FORBIDDEN);
+      throw new CustomException("REVIEW#3_001", "본인의 리뷰만 삭제할 수 있습니다.", HttpStatus.FORBIDDEN);
     }
   }
 }
