abe9c5dad5e9721e5f4b09d72f90622d338dee68
Younghan Yonn <dudgks1172@naver.com>
2024-12-11 18:21:35 +0900

    [#23] chore : 코드 형식 수정

--- src/main/java/com/sparta/travelconquestbe/api/coupon/service/CouponService.java
+++ src/main/java/com/sparta/travelconquestbe/api/coupon/service/CouponService.java
@@ -1,7 +1,5 @@
 package com.sparta.travelconquestbe.api.coupon.service;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-
 import com.sparta.travelconquestbe.api.coupon.dto.request.CouponCreateRequest;
 import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponCreateResponse;
 import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponSearchResponse;
@@ -14,6 +12,7 @@ import com.sparta.travelconquestbe.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -31,52 +30,30 @@ public class CouponService {
   public CouponCreateResponse createCoupon(CouponCreateRequest request, Long userId) {
 
     //유저가 관리자인지 확인(임시)
-    validateUser(userId);
-
-    Coupon coupon = getCoupon(request);
-
-    couponRepository.save(coupon);
-
-    return buildResponse(coupon);
-  }
-
-
-  //유저가 관리자인지 확인(임시)
-  public void validateUser(Long userId) {
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다"));
-
+    User user = userRepository.findById(userId).orElseThrow(
+        () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다"));
     if (!(user.getType().equals(UserType.ADMIN))) {
-      throw new CustomException("COUPON#1_002",
+      throw new CustomException("COUPON#3_001",
           "해당 리소스에 접근할 권한이 없습니다.",
-          FORBIDDEN);
+          HttpStatus.FORBIDDEN);
     }
-  }
 
-
-  // 쿠폰 엔티티 생성
-  private Coupon getCoupon(CouponCreateRequest request) {
-    Coupon coupon = Coupon.builder()
-        .name(request.getName())
-        .description(request.getDescription())
-        .type(request.getType())
-        .code(request.getCode())
-        .discountAmount(request.getDiscountAmount())
-        .validUntil(request.getValidUntil())
-        .count(request.getCount())
-        .build();
-    return coupon;
-  }
-
-  // 응답 생성
-  private CouponCreateResponse buildResponse(Coupon coupon) {
+    Coupon coupon =
+        Coupon.builder()
+            .name(request.getName())
+            .description(request.getDescription())
+            .type(request.getType())
+            .discountAmount(request.getDiscountAmount())
+            .validUntil(request.getValidUntil())
+            .count(request.getCount())
+            .build();
+    couponRepository.save(coupon);
 
     return CouponCreateResponse.builder()
         .id(coupon.getId())
         .name(coupon.getName())
         .descriotion(coupon.getDescription())
         .type(coupon.getType())
-        .code(coupon.getCode())
         .discountAmount(coupon.getDiscountAmount())
         .validUntil(coupon.getValidUntil())
         .count(coupon.getCount())
