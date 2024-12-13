84a5a4a834f57e21064e612abdf31110ee34e56d
lastdove <tpqms9917@gmail.com>
2024-12-13 17:53:25 +0900

    fix : 쿠폰 @AuthUser 수정

--- src/main/java/com/sparta/travelconquestbe/api/coupon/service/CouponService.java
+++ src/main/java/com/sparta/travelconquestbe/api/coupon/service/CouponService.java
@@ -3,6 +3,7 @@ package com.sparta.travelconquestbe.api.coupon.service;
 import com.sparta.travelconquestbe.api.coupon.dto.request.CouponCreateRequest;
 import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponCreateResponse;
 import com.sparta.travelconquestbe.api.coupon.dto.respones.CouponSearchResponse;
+import com.sparta.travelconquestbe.common.auth.AuthUserInfo;
 import com.sparta.travelconquestbe.common.exception.CustomException;
 import com.sparta.travelconquestbe.domain.coupon.entity.Coupon;
 import com.sparta.travelconquestbe.domain.coupon.repository.CouponRepository;
@@ -20,18 +21,14 @@ import org.springframework.stereotype.Service;
 public class CouponService {
 
   private final CouponRepository couponRepository;
-  private final UserRepository userRepository;
 
   public Page<CouponSearchResponse> searchAllCoupons(int page, int limit) {
 
     return couponRepository.searchAllCoupons(PageRequest.of(page - 1, limit));
   }
 
-  public CouponCreateResponse createCoupon(CouponCreateRequest request, Long userId) {
+  public CouponCreateResponse createCoupon(CouponCreateRequest request, AuthUserInfo user) {
 
-    //유저가 관리자인지 확인(임시)
-    User user = userRepository.findById(userId).orElseThrow(
-        () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다"));
     if (!(user.getType().equals(UserType.ADMIN))) {
       throw new CustomException("COUPON#3_001",
           "해당 리소스에 접근할 권한이 없습니다.",
