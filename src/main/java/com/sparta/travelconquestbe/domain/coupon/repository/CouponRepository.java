cb455eff59c1b7589f285dae92fc1ab45aee1d36
Younghan Yonn <dudgks1172@naver.com>
2024-12-17 18:04:23 +0900

    [#57] chore : 피드백 수정

--- src/main/java/com/sparta/travelconquestbe/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/sparta/travelconquestbe/domain/coupon/repository/CouponRepository.java
@@ -2,11 +2,8 @@ package com.sparta.travelconquestbe.domain.coupon.repository;
 
 import com.sparta.travelconquestbe.domain.coupon.entity.Coupon;
 import io.lettuce.core.dynamic.annotation.Param;
-import jakarta.persistence.LockModeType;
 import java.time.LocalDate;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,9 +12,4 @@ public interface CouponRepository extends JpaRepository<Coupon, Long>, CouponRep
   @Modifying
   @Query("DELETE FROM Coupon c WHERE c.validUntil < :currentDate")
   void deleteByExpiredCoupons(@Param("currentDate") LocalDate currentDate);
-
-  // 비관적 락 설정
-  @Lock(LockModeType.PESSIMISTIC_WRITE)
-  @Query("SELECT c FROM Coupon c WHERE c.id = :couponId")
-  Optional<Coupon> findByIdWithPessimisticLock(@Param("couponId") Long couponId);
 }
