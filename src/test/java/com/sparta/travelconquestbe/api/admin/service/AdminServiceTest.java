c249cabc176b1d08b4e049a0a505e2c4f89f068b
CHOSEONGJUN <lionjojo@naver.com>
2024-12-23 01:26:16 +0900

    [#98]refactor : 테스트 컨테이너활용 CI 구축

--- src/test/java/com/sparta/travelconquestbe/api/admin/service/AdminServiceTest.java
+++ src/test/java/com/sparta/travelconquestbe/api/admin/service/AdminServiceTest.java
@@ -14,16 +14,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 @Transactional
 @ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
 class AdminServiceTest extends TestContainerSupport {
 
   @Autowired
