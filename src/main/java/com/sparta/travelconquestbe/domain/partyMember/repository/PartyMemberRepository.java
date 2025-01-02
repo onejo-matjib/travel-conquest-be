5d46d068562b24301c9d7e9e3f0df5b36cae82b7
Younghan Yonn <dudgks1172@naver.com>
2025-01-02 23:35:32 +0900

    [#106] chore : 코드 충돌 처리

--- src/main/java/com/sparta/travelconquestbe/domain/partyMember/repository/PartyMemberRepository.java
+++ src/main/java/com/sparta/travelconquestbe/domain/partyMember/repository/PartyMemberRepository.java
@@ -4,6 +4,7 @@ import com.sparta.travelconquestbe.domain.partyMember.entity.PartyMember;
 import com.sparta.travelconquestbe.domain.partyMember.enums.MemberType;
 import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,6 +14,10 @@ public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> 
 
   Optional<PartyMember> findByUserIdAndPartyIdAndMemberType(Long userId, Long partyId,
       MemberType memberType);
+
+  Optional<PartyMember> findByUserIdAndPartyId(Long userId, Long partyId);
+
+  List<PartyMember> findByPartyIdAndMemberTypeNot(Long id, MemberType memberType);
 
   @Modifying
   @Transactional
