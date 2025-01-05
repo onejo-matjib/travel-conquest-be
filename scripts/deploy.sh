b8fa33667c3fbb91ccc4a62e0c17166bcd4a5aeb
lastdove <tpqms9917@gmail.com>
2025-01-06 05:07:16 +0900

    feat : CD Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
 cp $BUILD_JAR $DEPLOY_PATH 2>> /home/ubuntu/deploy_err.log
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
-sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15 2>> /home/ubuntu/deploy_err.log
+sudo pkill -f 'java -jar'
 
 DEPLOY_JAR=$DEPLOY_PATH/$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
